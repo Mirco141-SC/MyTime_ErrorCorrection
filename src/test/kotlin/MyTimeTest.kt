8999f6cb5b59727b8900ea615cf5b9a581ee62f9
Mirco141-SC <mirco.rossi.stud@ispascalcomandini.it>
2025-11-30 22:21:07 +0100

    Update MyTimeTest.kt
    What has been changed in this code:
    + Updated code files to respect all conventions;
    + Now, the class MyTime bases itself on the number of seconds that have passed since the first midnight in which day the single object was created on,
      basing on a single value instead of more than ones, making the process easier and similar to how Epoch Timestamps work;
    + Now, the number of seconds since the first midnight in which day the single object was created on is accessible as a property of the object, making this.convertToSeconds()
      useless;
    + Now, properties hours, minutes and seconds bases themselves on the number of seconds that have passed since the first midnight in which day the single object was created on
      making them calculated fields, and accessible as a property;
    + Added daysSinceFirstMidnight to count the days that have passed since the first day that the single object was created on;
    + Now the value of the seconds to add in addSeconds() requires the provided parameter to be higher than 0 because the function is useless if you're trying to add 0 seconds;
    + Corrected exceptions' messages grammar and readability;
    + Compacted into the "return" instruction the isPrevious() function;
    + Corrected differenceTime() function by swapping the objects comparison in the first instruction, and by adding a system to handle negative differences;
    + Compacted requires' conditions to range checks;
    + Re-written hashCode() and equals() functions to base themselves on the value of secondsSinceFirstMidnight for a more precise indication;
    - Removed useless this.convertToSeconds() function, replaced by this.secondsSinceFirstMidnight property;
    - Removed useless hours, minutes and seconds assignation in the addSeconds() function;
    - Removed setters tests, because the only setter in the class is private;
    - Removed convertToSeconds() function's tests: useless since function's removal;

--- src/test/kotlin/MyTimeTest.kt
+++ src/test/kotlin/MyTimeTest.kt
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.assertThrows
 
 
 class MyTimeTest {
-
-
    /************************** Constructor ********************************/
    @Test
    fun MyTime_hourLessThan0_IllegalArgumentException(){
@@ -34,134 +32,65 @@ class MyTimeTest {
 
 
    /************************** isPrevious ********************************/
-   //test per verificare il corretto comportamento della fun isPrevious
    @Test
    fun isPrevious_parameterTimeIsPrevius_returnFalse() {
        val time1 = MyTime(12,10,30)
        val time2 = MyTime(12,10,40)
 
-
        val result = time2.isPrevious(time1)
-
 
        assertEquals(false, result)
    }
-
 
    @Test
    fun isPrevious_parameterTimeIsNotPrevius_returnTrue() {
        val time1 = MyTime(12,10,30)
        val time2 = MyTime(12,10,40)
 
-
        val result = time1.isPrevious(time2)
-
 
        assertEquals(true, result)
    }
-
 
    @Test
    fun isPrevious_parameterTimeIsEquals_returnFalse() {
        val time1 = MyTime(12,10,30)
 
-
        val result = time1.isPrevious(time1)
-
 
        assertEquals(false, result)
    }
 
 
    /************************** SET ********************************/
-   //test per verificare il corretto comportamento del set
    @Test
-   fun setHour_valueIn023_hourUpdated() {
-       val time1 = MyTime(12,10,30)
+   fun getHours_undefined_hourUpdated() {
+        val time1 = MyTime(12,10,30)
 
+        val result = time1.hours
+        val expected = 12
 
-       val expected = 23
-       time1.hour=expected
-
-
-       val result = time1.hour
-       assertEquals(expected, result)
+        assertEquals(expected, result)
    }
-   //test per verificare la corretta gestione dei dati accettabili e non accettabili
+   
    @Test
-   fun setHour_valueLessThan0_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
+   fun getMinutes_undefined_hourUpdated() {
+        val time1 = MyTime(12,10,30)
 
+        val result = time1.minutes
+        val expected = 10
 
-       assertThrows<IllegalArgumentException> { time1.hour = -1 }
-   }
-   @Test
-   fun setHour_valueGreatherThan23_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
-
-
-       assertThrows<IllegalArgumentException> { time1.hour = 24 }
+        assertEquals(expected, result)
    }
 
-
-   //test per verificare il corretto comportamento del set
    @Test
-   fun setMinute_valueIn059_hourUpdated() {
-       val time1 = MyTime(12,10,30)
+   fun getSeconds_undefined_hourUpdated() {
+        val time1 = MyTime(12,10,30)
 
+        val result = time1.seconds
+        val expected = 30
 
-       val expected = 23
-       time1.minute=expected
-
-
-       val result = time1.minute
-       assertEquals(expected, result)
-   }
-   //test per verificare la corretta gestione dei dati accettabili e non accettabili
-   @Test
-   fun setMinute_valueLessThan0_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
-
-
-       assertThrows<IllegalArgumentException> { time1.minute = -1 }
-   }
-   @Test
-   fun setMinute_valueGreatherThan59_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
-
-
-       assertThrows<IllegalArgumentException> { time1.minute = 60 }
-   }
-
-
-   @Test
-   fun setSecond_valueIn059_hourUpdated() {
-       val time1 = MyTime(12,10,30)
-
-
-       val expected = 23
-       time1.second=expected
-
-
-       val result = time1.second
-       assertEquals(expected, result)
-   }
-   //test per verificare la corretta gestione dei dati accettabili e non accettabili
-   @Test
-   fun setSecond_valueLessThan0_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
-
-
-       assertThrows<IllegalArgumentException> { time1.second = -1 }
-   }
-
-
-   @Test
-   fun setSecond_valueGreatherThan59_IllegalArgumentException() {
-       val time1 = MyTime(12,10,30)
-
-
-       assertThrows<IllegalArgumentException> { time1.second = 60 }
+        assertEquals(expected, result)
    }
 
 
@@ -174,116 +103,100 @@ class MyTimeTest {
        assertThrows<IllegalArgumentException> { time1.addSeconds(-1) }
    }
 
-
    @Test
    fun addSeconds_timeToAddInDay_timeUpdated() {
        val time1 = MyTime(12,10,30)
 
-
        val addedSeconds = 45
        time1.addSeconds(addedSeconds)
 
-
        val expected = MyTime(12,11,15)
-
 
        assertEquals(expected, time1)
    }
-
 
    @Test
    fun addSeconds_timeToAddInNewDay_timeUpdated() {
        val time1 = MyTime(23,59,30)
 
-
+       val previous = time1.secondsSinceFirstMidnight
        val addedSeconds = 45
        time1.addSeconds(addedSeconds)
 
-
-       val expected = MyTime(0,1,15)
-
-
-       assertEquals(expected, time1)
+       assertEquals(previous + 45, time1.secondsSinceFirstMidnight)
+       assertEquals(1, time1.daysSinceFirstMidnight)
    }
 
-
-
-
-   /************************ convertInSecond *******************************/
-   @Test
-   fun convertInSecond_timezero_zero() {
-       val time1 = MyTime(0,0,0)
-
-
-       assertEquals(0,time1.convertInSecond())
-   }
-
-
-   @Test
-   fun convertInSecond_timeAcceptable_zero() {
-       val time1 = MyTime(1,1,1)
-
-
-       assertEquals(3661,time1.convertInSecond())
-   }
-
-
-
-
-
-
+   
+   /************************ differenceTime *******************************/
    @Test
    fun differenceTime_sameTime_zero() {
        val time1 = MyTime(1,1,1)
        val time2 = MyTime(1,1,1)
 
-
        assertEquals(0, time1.differenceTime(time2))
-
-
    }
-
 
    @Test
    fun differenceTime_parameterTimeAfter_differenceCalculetdToParameter() {
-
-
-       /*
-       tra le 2:00:00 e le 5:00:00 intercorrono 3 ore
-        */
        val time1 = MyTime(2,0,0)
        val time2 = MyTime(5,0,0)
-
 
        val expected = 3*3600
 
-
        assertEquals(expected, time1.differenceTime(time2))
-
-
    }
-
 
    @Test
    fun differenceTime_parameterTimePrevious_differenceCalculetdToDayAfter() {
-
-
-       /*
-       tra le 5:00:00 e le 2:00:00 intercorrono 21 ore
-        */
        val time1 = MyTime(2,0,0)
        val time2 = MyTime(5,0,0)
 
-
        val expected = 21*3600
-
 
        assertEquals(expected, time2.differenceTime(time1))
    }
 
+    @Test
+    fun differenceTime_acrossMidnight_maxDifference() {
+        val time1 = MyTime(0,0,0)
+        val time2 = MyTime(23,59,59)
 
+        assertEquals(86399, time1.differenceTime(time2))
+    }
 
+    @Test
+    fun differenceTime_acrossMidnight_minDifference() {
+        val time1 = MyTime(23,59,59)
+        val time2 = MyTime(0,0,0)
 
+        assertEquals(1, time1.differenceTime(time2))
+    }
 
+    
+    /************************ equals *******************************/
+    @Test
+    fun equals_timesAreEqual_returnTrue() {
+        val time1 = MyTime(10,0,0)
+        val time2 = MyTime(10,0,0)
 
+        assertEquals(true, time1 == time2)
+    }
+
+    @Test
+    fun equals_differentTypes_returnFalse() {
+        val time1 = MyTime(10,0,0)
+
+        assertEquals(false, time1.equals("Not a MyTime"))
+    }
+    
+
+    /************************ hashCode *******************************/
+    @Test
+    fun hashCode_timesAreEqual_returnTrue() {
+        val time1 = MyTime(10,0,0)
+        val time2 = MyTime(10,0,0)
+
+        assertEquals(time1.hashCode(), time2.hashCode())
+    }
 }
